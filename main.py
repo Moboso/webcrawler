5a48de83dfdebe012cbeeacc5bc371a796dd632f
Moboso <44112190+Moboso@users.noreply.github.com>
2026-06-03 19:04:55 +0100

    Librarian can now get the text of their home page as a string

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ class Librarian:
     def __init__(self):
         response = requests.get("https://libraryofbabel.app/random")
         print(response.url)
-        self._home = response.url.split('.')
-        self._location = self._home
-        # soup = BeautifulSoup(response.text, "html.parser")
+        self._url = response.url
+        self._home = self.get_location()
+        soup = BeautifulSoup(response.text, "html.parser")
+        html_page = soup.find_all("pre")[1]
+        self._text = str(html_page)[5:-6]
+
+    def get_location(self):
+        return self._url.split('.')
 
     def get_page(self):
-        return self._location[-1]
+        return self.get_location()[-1]
 
     def get_book(self):
-        return self._location[-2]
+        return self.get_location()[-2]
     
     def get_shelf(self):
-        return self._location[-3]
+        return self.get_location()[-3]
     
     def get_wall(self):
-        return self._location[-4]
-    
-    def get_hex(self):
-        return self._location[-5].split('/')[-1]
-
+        return self.get_location()[-4]
 
 if __name__ == "__main__":
     Adam = Librarian()
-    print(Adam.get_hex())
