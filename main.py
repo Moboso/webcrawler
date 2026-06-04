20a637327e12a57d405218e8961fe4e8cc748c66
Moboso <44112190+Moboso@users.noreply.github.com>
2026-06-04 20:23:00 +0100

    Turned getters and setters into properties for a more pythonic program

--- main.py
+++ main.py
@@ -7,40 +7,42 @@ WORDSET = {word.lower() for word in words.words()}
 
 class Librarian:
     def __init__(self):
-        response = requests.get("https://libraryofbabel.app/random")
-        self._set_url(response.url)
-        self._home = self.get_location()
-        self.set_text(response)
+        self._response = requests.get("https://libraryofbabel.app/random")
+        self.home = self.location
 
-    def set_text(self, response: requests.Response) -> str:
-        soup = BeautifulSoup(response.text, "html.parser")
-        html_text = soup.find_all("pre")[1] # Get the page text as a html Tag
-        self._text = str(html_text)[5:-6] # Turn the text into a string
+    @property
+    def url(self):
+        return self._response.url
 
-    def _set_url(self, new_url: str) -> None:
-        self._url = new_url
-
-    def get_text(self) -> str:
-        return self._text
-
-    def get_location(self) -> list[str]:
-        return self._url.split('.')
-
-    def get_page(self) -> int:
-        return int(self.get_location()[-1])
-
-    def get_book(self) -> int:
-        return int(self.get_location()[-2])
+    @property
+    def text(self) -> str:
+        soup = BeautifulSoup(self._response.text, "html.parser")
+        html_text = soup.find_all("pre")[1] # Get the page text as a Tag
+        return str(html_text)[5:-6] # Convert the text into a string
     
-    def get_shelf(self) -> int:
-        return int(self.get_location()[-3])
+    @property
+    def location(self) -> list[str]:
+        return self.url.split('.')
+
+    @property
+    def page(self) -> int:
+        return int(self.location[-1])
+
+    @property
+    def book(self) -> int:
+        return int(self.location[-2])
     
-    def get_wall(self) -> int:
-        return int(self.get_location()[-4])
+    @property
+    def shelf(self) -> int:
+        return int(self.location[-3])
+    
+    @property
+    def wall(self) -> int:
+        return int(self.location[-4])
 
 if __name__ == "__main__":
-    #Adam = Librarian()
-    #print(Adam.get_text())
+    Adam = Librarian()
+    print(Adam.home)
     if 'aple'.lower() in WORDSET:
         print("Huzzah!")
     else:
